a144a757a52a85b4fb38bf49e2d92dc2847d6fe8
肖峰 <xiaofeng@cestc.cn>
2022-08-06 09:10:06 +0800

    init

---
+++ src/main/java/com/example/liteflowdemo/LiteflowDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.liteflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteflowDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteflowDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/liteflowdemo/LiteflowDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liteflowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteflowDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
